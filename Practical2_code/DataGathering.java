b0a4c6ef25f632976a62391fb6e26162af91cd9e
Martins <martins.pukis@gmail.com>
2022-11-29 12:40:21 +0100

    working data gathering

--- Practical2_code/DataGathering.java
+++ Practical2_code/DataGathering.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,34 +11,38 @@ public class DataGathering  {
     int mutationRate;
     boolean crossover;
     String fileName;
-
+    String filepath;
     File file;
     FileWriter fw;
     PrintWriter pw;
+
 public DataGathering( int populationSize,Population population, int mutationRate, boolean crossover, String fileName,String filePath)throws IOException{
-    File file= new File(filePath+fileName);
-    FileWriter fw = new FileWriter(file);
-    PrintWriter pw = new PrintWriter(file);
+    this.filepath=filePath;  
+    this.file= new File(filepath+"\\"+fileName);
+    this.fw = new FileWriter(file);
+    this.pw = new PrintWriter(file);
     generation=0;
     this.populationSize=populationSize;
     this.population=population;
     this.mutationRate=mutationRate;
     this.crossover=crossover;
     this.fileName=fileName;
+    
 }
-   public void Header(){
-    pw.println("| size: " +populationSize+"| mutation rate: " +mutationRate+ "Crossover?:"+crossover); 
+   public void Header() throws IOException{
+     pw.println("| size: " +populationSize+"| mutation rate: " +mutationRate+ "| Crossover:"+crossover); 
    }
-   public void saveResults(){
-    pw.println("Generation: " +generation+'\n'+"induvidual in generation| fittnes value");
-    for (int i = 0; i < population.populationSize(); i++) {
+
+
+   public void saveResults() throws IOException{
+     pw.println("Generation:" +generation);
+     for(int i = 0; i < population.populationSize(); i++) {
         pw.println(i+" | "+population.population[i].getFitness());
     }
     generation++;
    }
- public void close(){
+ 
+   public void closeWriter(){
     pw.close();
- }
-   
-
+   }
 }
--- Practical2_code/test.java
+++ Practical2_code/test.java
@@ -1,6 +1,28 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 public class test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        int[] tet ={};
-       System.out.println("DDD"+ 2+'\n'+"lol");
+       char[] bruh ={'a','a','d'};
+       Population ind= new Population(100);
+       ind.initialize();
+       for (int i = 0; i < ind.populationSize(); i++) {
+        ind.population[i].setFitness(1);
+       }
+       DataGathering reader = new DataGathering(100, ind , 0, false, "results.txt", "C:\\Users\\marti\\Desktop\\Data");
+       reader.Header();
+       reader.saveResults();
+       reader.saveResults();
+       reader.closeWriter();
+  
+      // System.out.println( reader.file.getName());
+    //   File file= new File("C:\\Users\\marti\\Desktop\\Data\\results.txt");
+    //   FileWriter fw = new FileWriter(file);
+    //   PrintWriter pw = new PrintWriter(file);
+    //   pw.println("xdddddd");
+    //   pw.close();
+      
     }
 }
